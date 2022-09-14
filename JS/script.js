fa70ee4ef9fd6ab673a3b29344031bc601497bea
GodwinAsuquo <godwinedetauquo@gmail.com>
2022-09-14 07:11:22 +0100

    futher edit

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,12 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry=>{
         entry.target.classList.toggle('motion-safe:animate-fadeIn', entry.isIntersecting)
     })
-}, {
-    threshold: 0.5,
-}
+}, 
+
 );
 
 elements.forEach(element => {
-element.classList.add('opacity-0'),
+element.classList.add('opacity-0')
 observer.observe(element)
 })
 
